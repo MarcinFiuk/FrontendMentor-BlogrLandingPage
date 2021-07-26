7f4c3cad81b6e4485640988febf87f9d43d38f64
Marcin_Fi <fiukmarcinn@gmail.com>
2021-07-26 10:22:50 +0100

    footer mobile version finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Footer from './components/Footer/Footer';
+import Attribution from './components/Attribution/Attribution';
+
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <GlobalStyle />
+            <Footer />
+            <Attribution />
+        </div>
+    );
 }
 
 export default App;
